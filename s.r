28cdd9eb1d771a628f87a7899e202a8a7484e8a3
microm84 <98686441+microm84@users.noreply.github.com>
2022-02-19 15:43:12 +1300

    spliting strings

--- s.r
+++ s.r
@@ -3944,3 +3944,210 @@ converted <- s %>%
     str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") 
 pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
 str_detect(converted, pattern)
+
+#
+s <- c("5'10", "6'1")
+tab <- data.frame(x = s)
+library(dplyr)
+library(tidyr)
+tab %>% separate(x, c("feet", "inches"), sep = "'")
+
+tab %>% extract(x, c("feet", "inches"), regex = "(\\d)'(\\d{1,2})")
+
+#
+s <- c("5'10", "6'1\"","5'8inches")
+tab <- data.frame(x = s)
+tab %>% separate(x, c("feet","inches"), sep = "'", fill = "right")
+#test
+tab %>% separate(x, c("feet","inches"), sep = "'", fill = "left")
+#test
+tab %>% separate(x, c("feet","inches"), sep = "'")
+
+tab %>% extract(x, c("feet", "inches"), regex = "(\\d)'(\\d{1,2})")
+
+#case 1
+yes <- c("5", "6", "5")
+no <- c("5'", "5''", "5'4")
+s <- c(yes, no)
+library(stringr)
+str_replace(s, "^([4-7])$", "\\1'0")
+
+#case 2
+yes <- c("5", "6", "5")
+no <- c("5'", "5''", "5'4")
+s <- c(yes, no)
+library(stringr)
+str_replace(s, "^([4-7])'?$", "\\1'0")
+
+#case 5
+yes <- c("1,7", "1, 8", "2, " )
+no <- c("5,8", "5,3,2", "1.7")
+s <- c(yes, no)
+str_replace(s, "^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2")
+
+#trim the end space
+str_trim("5 ' 9 ")
+#trim the start and end
+str_trim(" 5 ' 9 ")
+#trim start
+str_trim(" 5 ' 9")
+
+#to lower case
+s <- c("Five feet eight inches")
+str_to_lower(s)
+
+#putting together
+library(dslabs)
+data(reported_heights)
+x <- as.numeric(reported_heights$height)
+
+not_inches_or_cm <- function(x, smallest = 50, tallest = 84){
+  inches <- suppressWarnings(as.numeric(x))
+  ind <- !is.na(inches) & 
+    ((inches >= smallest & inches <= tallest) |
+       (inches/2.54 >= smallest & inches/2.54 <= tallest))
+  !ind
+}
+
+problems <- reported_heights %>% 
+  filter(not_inches_or_cm(height)) %>%
+  pull(height)
+
+convert_format <- function(s){
+  s %>%
+    str_replace("feet|foot|ft", "'") %>% #convert feet symbols to '
+    str_replace_all("inches|in|''|\"|cm|and", "") %>%  #remove inches and other symbols
+    str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") %>% #change x.y, x,y x y
+    str_replace("^([56])'?$", "\\1'0") %>% #add 0 when to 5 or 6
+    str_replace("^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2") %>% #change european decimal
+    str_trim() #remove extra space
+}
+
+words_to_numbers <- function(s){
+  str_to_lower(s) %>%  
+    str_replace_all("zero", "0") %>%
+    str_replace_all("one", "1") %>%
+    str_replace_all("two", "2") %>%
+    str_replace_all("three", "3") %>%
+    str_replace_all("four", "4") %>%
+    str_replace_all("five", "5") %>%
+    str_replace_all("six", "6") %>%
+    str_replace_all("seven", "7") %>%
+    str_replace_all("eight", "8") %>%
+    str_replace_all("nine", "9") %>%
+    str_replace_all("ten", "10") %>%
+    str_replace_all("eleven", "11")
+}
+
+converted <- problems %>% words_to_numbers %>% convert_format
+remaining_problems <- converted[not_inches_or_cm(converted)]
+pattern <- "^[4-7]\\s*'\\s*\\d+\\.?\\d*$"
+index <- str_detect(remaining_problems, pattern)
+remaining_problems[!index]
+
+#
+library(dslabs)
+data(reported_heights)
+x <- as.numeric(reported_heights$height)
+
+not_inches <- function(x, smallest = 50, tallest = 84){
+  inches <- suppressWarnings(as.numeric(x))
+  ind <- is.na(inches) | inches < smallest | inches > tallest
+  ind
+}
+
+library(dplyr)
+problems <- reported_heights %>% 
+  filter(not_inches(height)) %>%
+  pull(height)
+
+pattern <- "^([4-7])\\s*'\\s*(\\d+\\.?\\d*)$"
+
+smallest <- 50
+tallest <- 84
+new_heights <- reported_heights %>% 
+  mutate(original = height, 
+         height = words_to_numbers(height) %>% convert_format()) %>%
+  extract(height, c("feet", "inches"), regex = pattern, remove = FALSE) %>% 
+  mutate_at(c("height", "feet", "inches"), as.numeric) %>%
+  mutate(guess = 12*feet + inches) %>%
+  mutate(height = case_when(
+    !is.na(height) & between(height, smallest, tallest) ~ height, #inches 
+    !is.na(height) & between(height/2.54, smallest, tallest) ~ height/2.54, #centimeters
+    !is.na(height) & between(height*100/2.54, smallest, tallest) ~ height*100/2.54, #meters
+    !is.na(guess) & inches < 12 & between(guess, smallest, tallest) ~ guess, #feet'inches
+    TRUE ~ as.numeric(NA))) %>%
+  select(-guess)
+
+new_heights %>%
+  filter(not_inches(original)) %>%
+  select(original, height) %>% 
+  arrange(height) %>%
+  View()
+
+new_heights %>% arrange(height) %>% head(n=7)
+
+#split string
+filename <- system.file("extdata/murders.csv", package = "dslabs")
+lines <- readLines(filename)
+head(lines)
+
+library(dplyr)
+library(stringr)
+x <- str_split(lines, ",") 
+x %>% head()
+
+col_names <- x[[1]]
+x <- x[-1]
+
+library(purrr)
+map(x, function(y) y[1]) %>%
+head()
+
+#test
+map(x, function(i) {i[1]}) %>%
+head()
+
+map(x, 1) %>% head()
+
+#test
+filename <- system.file("extdata/murders.csv", package = "dslabs")
+lines <- readLines(filename)
+library(dplyr)
+library(stringr)
+x <- str_split(lines, ",")
+col_names <- x[[1]]
+x <- x[-1]
+library(purrr)
+library(tibble)
+dat <- tibble(map_chr(x, 1),  
+              map_chr(x, 2),
+              map_chr(x, 3),
+              map_chr(x, 4),
+              map_chr(x, 5)) %>%
+              setNames(col_names)
+
+library(readr)
+d = tibble(map_chr(x, 1),  
+              map_chr(x, 2),
+              map_chr(x, 3),
+              map_chr(x, 4),
+              map_chr(x, 5)) %>%
+              mutate_all(parse_guess) %>% 
+              setNames(col_names)
+
+#method 2
+filename <- system.file("extdata/murders.csv", package = "dslabs")
+lines <- readLines(filename)
+library(dplyr)
+library(stringr)
+x <- str_split(lines, ",", simplify = TRUE)
+col_names <- x[1,]
+x <- x[-1,]
+colnames(x) = col_names
+library(tibble)
+library(readr)
+x %>% 
+as_tibble() %>%
+mutate_all(parse_guess)
+
