311d325036938132c4d34b2c5867668171e309a7
microm84 <98686441+microm84@users.noreply.github.com>
2022-04-02 13:01:38 +1300

    section 6!!

--- s.r
+++ s.r
@@ -6564,136 +6564,191 @@ merged7 %>% lm(CashRatio ~ LnAssets + LnAge + DARatio + DivPayer + LagReturn + f
 merged7 %>% lm(CashRatio ~ LnAssets + LnAge + DARatio + DivPayer +
  LagReturn + factor(SICIndustry) + factor(Year), data = .) %>% summary()
 
+# machine learning 4.3
+# 4.3.2. Naive Bayes
+# Generating train and test set
+library("caret")
+data("heights")
+y <- heights$height
+set.seed(2)
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+train_set <- heights %>% slice(-test_index)
+test_set <- heights %>% slice(test_index)
 
+# Estimating averages and standard deviations
+params <- train_set %>%
+ group_by(sex) %>%
+ summarize(avg = mean(height), sd = sd(height))
+params
 
+# Estimating the prevalence
+pi <- train_set %>% summarize(pi=mean(sex=="Female")) %>% pull(pi)
+pi
 
+# Getting an actual rule
+x <- test_set$height
+f0 <- dnorm(x, params$avg[2], params$sd[2])
+f1 <- dnorm(x, params$avg[1], params$sd[1])
+p_hat_bayes <- f1*pi / (f1*pi + f0*(1 - pi))
 
+# 4.3.3. Controlling prevalence
+# Computing sensitivity
+y_hat_bayes <- ifelse(p_hat_bayes > 0.5, "Female", "Male")
+sensitivity(data = factor(y_hat_bayes), reference = factor(test_set$sex))
 
+# Computing specificity
+specificity(data = factor(y_hat_bayes), reference = factor(test_set$sex))
 
+# Changing the cutoff of the decision rule
+p_hat_bayes_unbiased <- f1 * 0.5 / (f1 * 0.5 + f0 * (1 - 0.5))
+y_hat_bayes_unbiased <- ifelse(p_hat_bayes_unbiased > 0.5, "Female", "Male")
+sensitivity(data = factor(y_hat_bayes_unbiased), reference = factor(test_set$sex))
+specificity(data = factor(y_hat_bayes_unbiased), reference = factor(test_set$sex))
 
-#length(unique(merged7$SICIndustry))
+# Draw plot
+qplot(x, p_hat_bayes_unbiased, geom = "line") +
+ geom_hline(yintercept = 0.5, lty = 2) +
+ geom_vline(xintercept = 67, lty = 2)
 
-#test
-a = data.frame(Asset = c(100, 200), Cash = c(200, 300), Loan = c(150, 200))
-a %>% select(Asset, Cash) %>% summarize_all(Mean = mean(), Sd = sd())
-a %>% as_tibble() %>% summarize_all(mean(), sd())
+#4.3.4. qda and lda
+# Load data
+data("mnist_27")
 
+# Estimate parameters from the data
+params <- mnist_27$train %>%
+ group_by(y) %>%
+ summarize(avg_1 = mean(x_1), avg_2 = mean(x_2),
+        sd_1 = sd(x_1), sd_2 = sd(x_2),
+        r = cor(x_1, x_2))
 
-a = data.frame(Asset = c(100, 200), Cash = c(200, 300), Loan = c(150, 200))
-a %>% summarize_all(funs(mean))
-a %>% summarize_all(c(mean,sd))
+# Contour plots
+mnist_27$train %>% mutate(y = factor(y)) %>%
+ ggplot(aes(x_1, x_2, fill = y, color = y)) +
+ geom_point(show.legend = FALSE) +
+ stat_ellipse(type="norm", lwd = 1.5)
 
-#1
-fundamentals = read.csv('fundamentals.csv', header = TRUE, sep = ',')
-market = read.csv('market.csv', header = TRUE, sep = ',')
+# Fit model
+library(caret)
+train_qda <- train(y ~., method = "qda", data = mnist_27$train)
+# Obtain predictors and accuracy
+y_hat <- predict(train_qda, mnist_27$test)
+confusionMatrix(data = y_hat, reference = mnist_27$test$y)$overall["Accuracy"]
+
+# Draw separate plots for 2s and 7s
+mnist_27$train %>% mutate(y = factor(y)) %>%
+ ggplot(aes(x_1, x_2, fill = y, color = y)) +
+ geom_point(show.legend = FALSE) +
+ stat_ellipse(type="norm") +
+ facet_wrap(~y)
+
+params <- mnist_27$train %>%
+ group_by(y) %>%
+ summarize(avg_1 = mean(x_1), avg_2 = mean(x_2),
+        sd_1 = sd(x_1), sd_2 = sd(x_2),
+        r = cor(x_1, x_2))
+params <- params %>% mutate(sd_1 = mean(sd_1), sd_2 = mean(sd_2), r = mean(r))
+train_lda <- train(y ~., method = "lda", data = mnist_27$train)
+y_hat <- predict(train_lda, mnist_27$test)
+confusionMatrix(data = y_hat, reference = mnist_27$test$y)$overall["Accuracy"]
+
+#exercise
+library(dslabs)
+library(caret)
 library(dplyr)
-merged = inner_join(fundamentals, market, by =c('FirmID', 'Year'))
-#2
-merged2 = merged %>% mutate(CashRatio = Cash / Assets, DARatio = LTDebt / Assets,
-  ROA = EBITDA / Assets, DivPayer = ifelse(Dividends > 0, 1, 0),
-  LnAssets = log(Assets * 10^6), LnAge = log(Age))
-#3 We need school R
-#library(vtable)
-m3 = merged2 %>% select(Age, Assets, Cash, EBITDA, Equity, Liabilities,
-  LTDebt, MarketCap, NetIncome, Return, Revenue, CashRatio, DARatio,
-  DivPayer, ROA) %>% summarize_all(c(function(.){n()}, mean, sd, min, median, max)) %>% matrix(15,6)
-colnames(m3) = c('N', 'Mean', 'Sd', 'Min', 'Median', 'Max')
-rownames(m3) = c('Age', 'Assets', 'Cash', 'EBITDA', 'Equity', 'Liabilities',
-  'LTDebt', 'MarketCap', 'NetIncome', 'Return', 'Revenue', 'CashRatio', 'DARatio',
-   'DivPayer', 'ROA')
+data("tissue_gene_expression")
+   
+# set.seed(1993) #if using R 3.5 or earlier
+set.seed(1993, sample.kind="Rounding") # if using R 3.6 or later
+ind <- which(tissue_gene_expression$y %in% c("cerebellum", "hippocampus"))
+y <- droplevels(tissue_gene_expression$y[ind])
+x <- tissue_gene_expression$x[ind, ]
+x <- x[, sample(ncol(x), 10)]
+#q1
+train(x, y, method = 'lda')
+fit_lda <- train(x, y, method = "lda")
+fit_lda$results["Accuracy"]
+#q2
+t(fit_lda$finalModel$means) %>% data.frame() %>%
+	mutate(predictor_name = rownames(.)) %>%
+	ggplot(aes(cerebellum, hippocampus, label = predictor_name)) +
+	geom_point() +
+	geom_text() +
+	geom_abline()
+#alternative
+fit_lda$finalModel$mean
+rank(fit_lda$finalModel$mean['hippocampus',])
+rank(fit_lda$finalModel$mean['cerebellum',])
 
-m2 = matrix(1:75, 15, 5)
-m2[1,1] = NA
-m2 %>% as.data.frame() %>% summarize_all(c(function(.){sum(!is.na(.))}, mean))
+#q3
+set.seed(1993, sample.kind="Rounding") # if using R 3.6 or later
+ind <- which(tissue_gene_expression$y %in% c("cerebellum", "hippocampus"))
+y <- droplevels(tissue_gene_expression$y[ind])
+x <- tissue_gene_expression$x[ind, ]
+x <- x[, sample(ncol(x), 10)]
+fit_qda <- train(x, y, method = "qda")
+fit_qda$results["Accuracy"]
+#q4
+fit_qda$finalModel$means
+rank(fit_qda$finalModel$mean['hippocampus',])
+rank(fit_qda$finalModel$mean['cerebellum',])
 
-#test can delete later
-a=1:1000
-a2=1001:2000
-d = data.frame(a, a2)
-r = d %>% summarize_all(quantile) %>% as.matrix() %>% t()
+t(fit_qda$finalModel$means) %>% data.frame() %>%
+	mutate(predictor_name = rownames(.)) %>%
+	ggplot(aes(cerebellum, hippocampus, label = predictor_name)) +
+	geom_point() +
+	geom_text() +
+	geom_abline()
 
+#5
+library(dslabs)
+library(caret)
+library(dplyr)
+data("tissue_gene_expression")
+   
+# set.seed(1993) #if using R 3.5 or earlier
+set.seed(1993, sample.kind="Rounding") # if using R 3.6 or later
+ind <- which(tissue_gene_expression$y %in% c("cerebellum", "hippocampus"))
+y <- droplevels(tissue_gene_expression$y[ind])
+x <- tissue_gene_expression$x[ind, ]
+x <- x[, sample(ncol(x), 10)]
 
+fit_lda <- train(x, y, method = "lda", preProcess = 'center')
+fit_lda$results["Accuracy"]
 
-
-
-mt = merged2 %>% select(Age, Assets, Cash, EBITDA, Equity, Liabilities,
-  LTDebt, MarketCap, NetIncome, Return, Revenue, CashRatio, DARatio,
-  DivPayer, ROA) %>% summarize_all(~n())
-
-m3 = merged2 %>% select(Age, Assets, Cash, EBITDA, Equity, Liabilities,
-  LTDebt, MarketCap, NetIncome, Return, Revenue, CashRatio, DARatio,
-  DivPayer, ROA) %>% summarize_all(c(function(.){n()}, mean, sd, 
-  function(.){quantile(., 0)}, function(.){quantile(., 0.25)}, 
-  function(.){quantile(., 0.5)}, function(.){quantile(., 0.75)}, 
-  function(.){quantile(., 1)})) %>% round(2) %>% matrix(15, 8)
-colnames(m3) = c('N', 'Mean', 'Sd', 'Min', 'Pctl.25', 'Median', 'Pctl.75', 'Max')
-rownames(m3) = c('Age', 'Assets', 'Cash', 'EBITDA', 'Equity', 'Liabilities',
-  'LTDebt', 'MarketCap', 'NetIncome', 'Return', 'Revenue', 'CashRatio', 'DARatio',
-  'DivPayer', 'ROA')
-m3
-
-
-
-
-
-
-
-
-
-
-
-b=quantile(a)
-matrix(b)
-> str(b)
- Named num [1:5] 1 251 500 750 1000
- - attr(*, "names")= chr [1:5] "0%" "25%" "50%" "75%" ...
-> a%>%summarize(quantile)
-Error in UseMethod("summarise") : 
-  no applicable method for 'summarise' applied to an object of class "c('integer', 'numeric')"
-> a=data.frame(a)
-
-> 
-> a%>%summarize(quantile)
-Error in `summarize()`:
-! Problem while computing `..1 = quantile`.
-✖ `..1` must be a vector, not a function.
-Run `rlang::last_error()` to see where the error occurred.
-> library(dplyr)
-> a%>%summarize(quantile)
-Error in `summarize()`:
-! Problem while computing `..1 = quantile`.
-✖ `..1` must be a vector, not a function.
-Run `rlang::last_error()` to see where the error occurred.
-> a%>%summarize_all(quantile)
-        a
-1    1.00
-2  250.75
-3  500.50
-4  750.25
-5 1000.00
-> b=a%>%summarize_all(quantile)
-> str(b)
-'data.frame':	5 obs. of  1 variable:
- $ a: Named num  1 251 500 750 1000
-  ..- attr(*, "names")= chr [1:5] "0%" "25%" "50%" "75%" ...
-> c(b)
-$a
-     0%     25%     50%     75%    100% 
-   1.00  250.75  500.50  750.25 1000.00 
-
-> b1=c(b)
-> str(b1)
-List of 1
- $ a: Named num [1:5] 1 251 500 750 1000
-  ..- attr(*, "names")= chr [1:5] "0%" "25%" "50%" "75%" ...
-> b1
-$a
-     0%     25%     50%     75%    100% 
-   1.00  250.75  500.50  750.25 1000.00 
-
-> class(b1)
-[1] "list"
-> 
+t(fit_lda$finalModel$mean) %>% data.frame() %>% mutate(dif = hippocampus - cerebellum)
 
 #
-#
+library(dslabs)
+library(caret)
+library(dplyr)
+data("tissue_gene_expression")
+   
+# set.seed(1993) #if using R 3.5 or earlier
+set.seed(1993, sample.kind="Rounding") # if using R 3.6 or later
+ind <- which(tissue_gene_expression$y %in% c("cerebellum", "hippocampus"))
+y <- droplevels(tissue_gene_expression$y[ind])
+x <- tissue_gene_expression$x[ind, ]
+x <- x[, sample(ncol(x), 10)]
+
+fit_lda <- train(x, y, method = "lda", preProcess = "center")
+fit_lda$results["Accuracy"]
+t(fit_lda$finalModel$means) %>% data.frame() %>%
+	mutate(predictor_name = rownames(.)) %>%
+	ggplot(aes(predictor_name, hippocampus)) +
+	geom_point() +
+	coord_flip()
+
+#6
+
+library(dslabs)      
+library(caret)
+data("tissue_gene_expression")
+           
+# set.seed(1993) # if using R 3.5 or earlier
+set.seed(1993, sample.kind="Rounding") # if using R 3.6 or later
+y <- tissue_gene_expression$y
+x <- tissue_gene_expression$x
+x <- x[, sample(ncol(x), 10)]
+
+fit_lda <- train(x, y, method = "lda", preProcess = "center")
+fit_lda$results["Accuracy"]
