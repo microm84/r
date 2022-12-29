275f75b8f0fe0d75f5f904ed64645580f9f09644
microm84 <98686441+microm84@users.noreply.github.com>
2022-12-30 04:21:07 +1300

    ha

--- thesis.r
+++ thesis.r
@@ -1,4 +1,4 @@
-# Model 1.1 - 1.6
+# Model 1 (Model 1 in results.r)
 # getwd()
 library(dplyr)
 library(readr)
@@ -17,3 +17,30 @@ mutate_at(vars(estimate, statistic), ~ round(., digits = 3)) %>%
 mutate(result = paste0(estimate, significance, "\n(", statistic, ")")) %>% select(term, result) %>% write_csv("f_1.csv")
 nrow(m_data)
 fit %>% glance() %>% select(r.squared) %>% round(digits = 3) %>% write_csv("f_2.csv")
+
+# Model 3.1 - 3.4
+library(dplyr)
+library(readr)
+library(tidyr)
+library(purrr)
+library(broom)
+sample = read_csv("sample_3.csv")
+formulas = list(leverage ~ major_customer + bm + e_index + female_ceo + growth + intangible + pay_turn + roa + size + tangibility, 
+leverage ~ major_customer + bm + e_index + female_ceo + growth + intangible + pay_turn + roa + size + tangibility
+ + factor(industry), 
+leverage ~ major_customer + bm + e_index + female_ceo + growth + intangible + pay_turn + roa + size + tangibility
+ + factor(fyear), 
+leverage ~ major_customer + bm + e_index + female_ceo + growth + intangible + pay_turn + roa + size + tangibility
+ + factor(industry) + factor(fyear))
+f_1 = function(x) {
+    lm(x, sample) %>% tidy() %>% 
+    mutate(significance = symnum(p.value, cutpoints = c(0, 0.01, 0.05, 0.1, 1), symbols = c("***", "**", "*", ""))) %>% 
+    mutate_at(vars(estimate, statistic), ~ round(., digits = 3)) %>% 
+    mutate(result = paste0(estimate, significance, "\n(", statistic, ")")) %>% select(term, result)
+}
+map_dfr(formulas, f_1) %>% write_csv("f_1.csv")
+nrow(sample)
+f_2 = function(x) {
+    lm(x, sample) %>% glance() %>% select(r.squared) %>% round(digits = 3)
+}
+map_dfr(formulas, f_2) %>% write_csv("f_2.csv")
